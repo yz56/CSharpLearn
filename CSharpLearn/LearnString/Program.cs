5460e40983995baa1e2c5005a8af0834099b8938
yz56 <yiming.zhang@thoughtworks.com>
2020-10-16 16:20:09 +0800

    first commit

---
+++ CSharpLearn/LearnString/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LearnString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("ssss");
+        }
+    }
+}
